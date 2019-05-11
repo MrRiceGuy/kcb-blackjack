c48372968bbd60cdfc87ed3e591e8a8dc9ff3ac2
Robert Baxter <baxterrp@gmail.com>
2019-05-11 11:55:51 -0400

    created register user end point

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,5 +1,11 @@
 module.exports = {
     RenderView : function(req, res){
         res.render('registration', {});
+    },
+    RegisterNewUser : function(req, res){
+        res.send({
+            message : 'received new user info',
+            user : req.body
+        });
     }
 }
