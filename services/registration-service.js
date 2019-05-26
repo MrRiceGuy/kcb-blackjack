bfda56c9ea061994de5a29ae2eb3e8ae4839c7f9
Robert Baxter <baxterrp@gmail.com>
2019-05-26 10:07:32 -0400

    inserting code to user_code table upon registration

--- services/registration-service.js
+++ services/registration-service.js
@@ -1,4 +1,5 @@
 var cruddy = require('cruddy-orm');
+var moment = require('moment');
 var utility = require('./utility.js');
 
 module.exports = {
@@ -6,17 +7,25 @@ module.exports = {
         if(!(utility.ValidateUser(user)))
             throw "Invalid user entered";
         
+        var userid = generateCode('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx');
         cruddy.Insert('users', 
-            [generateUserId(), user.firstName, user.lastName, user.email, user.username, user.birthdate, user.password], 
+            [userid, user.firstName, user.lastName, user.email, user.username, user.birthdate, user.password], 
             ['id', 'first_name', 'last_name', 'email', 'username', 'birthdate', 'password'], 
             function(response){
-                callback(response);
+                if(response.success){
+                    cruddy.Insert('user_code', 
+                        [userid, generateCode('xxxxxx'), moment().format('YYYY-MM-DD HH:mm:ss')], 
+                        ['account_id', 'code', 'date'], 
+                        function(response){
+                            callback(response);
+                        });
+                }
         });
     }
 }
 
-function generateUserId(){
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+function generateCode(format){
+    return format.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
